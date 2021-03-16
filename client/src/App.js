56bf57369dfa8c7909f86ef0f9a44c0447fecc76
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2021-03-16 00:43:57 -0400

    indent

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import {
   fetchDetailedStats,
   fetchTestings,
   fetchCountyTotalsDB,
-  fetchStatuses,
+  fetchUpdatedTestings,
 } from "./api";
 import Aux from "./hoc/_Aux";
 import "./assets/scss/style.scss";
@@ -23,26 +23,28 @@ import Charts from "./components/Charts/Charts";
 import CampusCharts from "./components/CampusCharts/CampusCharts";
 import Status from "./components/Status/Status";
 import CampusCards from "./components/CampusCards/CampusCards";
+import TestingChart from "./components/TestingChart/TestingChart";
 
 // Images + accessories
 import PingryAnywhere from "./assets/images/PingryAnywhere";
 import Spinner from "react-spinkit";
 import FadeIn from "react-fade-in";
 import cx from "classnames";
+// import Testing from "../../models/testing.model";
 
 class App extends Component {
-  // // LEVELS
-  // // 1 --> open
-  // // 2 --> reduced density
-  // // 3 --> fully remote
-  // // 0 --> special (atm: Fully Remote for Re-Entry)
-  // static SHORT_HILLS_LEVEL = 1;
-  // static BASKING_RIDGE_LEVEL = 3;
+  // LEVELS
+  // 1 --> open
+  // 2 --> reduced density
+  // 3 --> fully remote
+  // 0 --> special (atm: Fully Remote for Re-Entry)
+  static SHORT_HILLS_LEVEL = 1;
+  static BASKING_RIDGE_LEVEL = 1;
 
-  // // TESTING RESULTS
-  // static POTENTIALLY_POSITIVE_PAIRS = 1;
-  // static TOTAL_NUM_TESTS = 1433;
-  // static SAMPLE_COLLECTION_DATE = new Date(2020, 10, 16);
+  // TESTING RESULTS
+  static POTENTIALLY_POSITIVE_PAIRS = 1;
+  static TOTAL_NUM_TESTS = 1433;
+  static SAMPLE_COLLECTION_DATE = new Date(2020, 10, 16);
 
   static MONTHS = [
     "January",
@@ -71,8 +73,11 @@ class App extends Component {
       loading: true,
       detailedStats: [],
       fetchedTestingData: [],
+      fetchedUpdatedTestingData: [],
       showMore: false,
-      fetchedStatuses: { baskingRidge: 1, shortHills: 1 },
+      entriesShown: 3,
+      priorTestingShown: false,
+      archiveEntriesShown: 3,
     };
     ReactGA.initialize("UA-177348263-1");
     ReactGA.initialize("UA-186911504-1");
@@ -87,7 +92,7 @@ class App extends Component {
     const fetchedDetailedStats = (await fetchDetailedStats())[0];
     const fetchedTestingData = (await fetchTestings())[0].data;
     const fetchedCountyTotals = await fetchCountyTotalsDB();
-    const fetchedStatuses = (await fetchStatuses())[0];
+    const fetchedUpdatedTestingData = (await fetchUpdatedTestings())[0].data;
     this.setState({
       statewideData: fetchedStatewideData,
       countyData: fetchedCountyData,
@@ -98,8 +103,10 @@ class App extends Component {
       loading: false,
       detailedStats: fetchedDetailedStats,
       fetchedTestingData: fetchedTestingData,
+      fetchedUpdatedTestingData: fetchedUpdatedTestingData,
       showMore: false,
-      fetchedStatuses: fetchedStatuses,
+      entriesShown: 3,
+      priorTestingShown: false,
     });
   }
 
@@ -109,14 +116,128 @@ class App extends Component {
   }
 
   render() {
-    let icon = (
+    let showMoreButton = (
       <button
         className={styles.testingButton}
-        onClick={() => this.setState({ showMore: !this.state.showMore })}
+        onClick={() =>
+          this.setState({
+            showMore: !this.state.showMore,
+          })
+        }
       >
         {this.state.showMore ? "Show Less" : "Show More"}
       </button>
     );
+
+    let showMoreRowsButton = (
+      <button
+        style={{ marginRight: "1rem", marginTop: "0.5rem" }}
+        className={styles.testingButton}
+        onClick={() =>
+          this.setState({
+            // Pick nearest multiple of 10 or maximum number of rows
+            entriesShown: Math.min(
+              this.state.entriesShown + 10,
+              this.state.fetchedUpdatedTestingData.length
+            ),
+          })
+        }
+        disabled={
+          this.state.entriesShown >= this.state.fetchedUpdatedTestingData.length
+        }
+      >
+        Show{" "}
+        {Math.max(
+          0,
+          Math.min(
+            this.state.entriesShown + 10,
+            this.state.fetchedUpdatedTestingData.length
+          ) - this.state.entriesShown
+        )}{" "}
+        More
+      </button>
+    );
+
+    let showFewerRowsButton = (
+      <button
+        // style={{ marginRight: "1rem", marginTop: "0.5rem" }}
+        className={styles.testingButton}
+        onClick={() =>
+          this.setState({
+            entriesShown: Math.max(3, this.state.entriesShown - 10),
+          })
+        }
+        disabled={this.state.entriesShown <= 3}
+      >
+        Show{" "}
+        {this.state.entriesShown - Math.max(3, this.state.entriesShown - 10)}{" "}
+        Fewer
+      </button>
+    );
+
+    let showArchiveData = (
+      <button
+        className={styles.testingButton}
+        onClick={() =>
+          this.setState({
+            priorTestingShown: !this.state.priorTestingShown,
+          })
+        }
+      >
+        {this.state.priorTestingShown ? "Hide" : "Show"} Archived Data
+      </button>
+    );
+
+    let showMoreRowsArchiveButton = (
+      <button
+        style={{ marginRight: "1rem", marginTop: "0.5rem" }}
+        className={styles.testingButton}
+        onClick={() =>
+          this.setState({
+            // Pick nearest multiple of 10 or maximum number of rows
+            archiveEntriesShown: Math.min(
+              this.state.archiveEntriesShown + 10,
+              this.state.fetchedTestingData.length
+            ),
+          })
+        }
+        disabled={
+          this.state.archiveEntriesShown >= this.state.fetchedTestingData.length
+        }
+      >
+        Show{" "}
+        {Math.max(
+          0,
+          Math.min(
+            this.state.archiveEntriesShown + 10,
+            this.state.fetchedTestingData.length
+          ) - this.state.archiveEntriesShown
+        )}{" "}
+        More
+      </button>
+    );
+
+    let showFewerRowsArchiveButton = (
+      <button
+        // style={{ marginRight: "1rem", marginTop: "0.5rem" }}
+        className={styles.testingButton}
+        onClick={() =>
+          this.setState({
+            archiveEntriesShown: Math.max(
+              3,
+              this.state.archiveEntriesShown - 10
+            ),
+          })
+        }
+        disabled={this.state.archiveEntriesShown <= 3}
+      >
+        Show{" "}
+        {this.state.archiveEntriesShown -
+          Math.max(3, this.state.archiveEntriesShown - 10)}{" "}
+        Fewer
+      </button>
+    );
+
     return (
       <Aux>
         {this.state.loading ? (
@@ -164,8 +285,8 @@ class App extends Component {
                 </a>
               </Col>
               <Status
-                shortHills={this.state.fetchedStatuses.shortHills}
-                baskingRidge={this.state.fetchedStatuses.baskingRidge}
+                shortHills={App.SHORT_HILLS_LEVEL}
+                baskingRidge={App.BASKING_RIDGE_LEVEL}
               />
 
               <Row>
@@ -174,6 +295,201 @@ class App extends Component {
                     <Card.Body>
                       <h6 className={cx("mb-4", styles.smallmargin)}>
                         Pingry's Return-to-School COVID-19 Testing
+                      </h6>
+                      <div className="row d-flex align-items-center">
+                        <div className="col-12">
+                          <h3 className="f-w-300 d-flex align-items-center m-b-0">
+                            <div className={cx(styles.center, styles.testing)}>
+                              <br />
+                              <table className={cx("table", styles.table)}>
+                                <thead>
+                                  <tr>
+                                    <th
+                                      className={cx(
+                                        styles.tableHeader,
+                                        styles.hoverable
+                                      )}
+                                    >
+                                      <p>Date of Sample Collection</p>
+                                    </th>
+                                    <th
+                                      className={cx(
+                                        styles.tableHeader,
+                                        styles.hoverable
+                                      )}
+                                    >
+                                      <p>Total Tests Administered</p>
+                                    </th>
+                                    <th
+                                      className={cx(
+                                        styles.tableHeader,
+                                        styles.hoverable,
+                                        styles.separator
+                                      )}
+                                    >
+                                      <p>Total Positive Cases</p>
+                                    </th>
+                                    <th
+                                      className={cx(
+                                        styles.tableHeader,
+                                        styles.hoverable
+                                      )}
+                                    >
+                                      <p>Basking Ridge Students</p>
+                                    </th>
+                                    <th
+                                      className={cx(
+                                        styles.tableHeader,
+                                        styles.hoverable,
+                                        styles.dottedRight
+                                      )}
+                                    >
+                                      <p>Basking Ridge Faculty & Staff</p>
+                                    </th>
+                                    <th
+                                      className={cx(
+                                        styles.tableHeader,
+                                        styles.hoverable
+                                      )}
+                                    >
+                                      <p>Short Hills Students</p>
+                                    </th>
+                                    <th
+                                      className={cx(
+                                        styles.tableHeader,
+                                        styles.hoverable
+                                      )}
+                                    >
+                                      <p>Short Hills Faculty & Staff</p>
+                                    </th>
+                                  </tr>
+                                </thead>
+                                <tbody className={styles.smallerText}>
+                                  {this.state.fetchedUpdatedTestingData.map(
+                                    (entry, i) => {
+                                      const date = new Date(entry.date);
+                                      return (
+                                        <tr
+                                          style={{
+                                            display:
+                                              i < this.state.entriesShown
+                                                ? "table-row"
+                                                : "none",
+                                          }}
+                                          key={i}
+                                        >
+                                          <td
+                                            className={cx(
+                                              styles.smallPadding,
+                                              styles.hoverable
+                                            )}
+                                          >
+                                            {date.getMonth() +
+                                              1 +
+                                              "/" +
+                                              date.getDate()}
+                                          </td>
+                                          <td
+                                            className={cx(
+                                              styles.smallPadding,
+                                              styles.hoverable
+                                            )}
+                                          >
+                                            {this.formatNum(entry.numTests)}
+                                          </td>
+                                          <td
+                                            className={cx(
+                                              styles.smallPadding,
+                                              styles.hoverable,
+                                              styles.separator
+                                            )}
+                                          >
+                                            {this.formatNum(
+                                              entry.totalPositiveCases
+                                            )}
+                                          </td>
+                                          <td
+                                            className={cx(
+                                              styles.smallPadding,
+                                              styles.hoverable
+                                            )}
+                                          >
+                                            {this.formatNum(
+                                              entry.baskingRidgeStudents
+                                            )}
+                                          </td>
+                                          <td
+                                            className={cx(
+                                              styles.smallPadding,
+                                              styles.hoverable,
+                                              styles.dottedRight
+                                            )}
+                                          >
+                                            {this.formatNum(
+                                              entry.baskingRidgeFacultyStaff
+                                            )}
+                                          </td>
+                                          <td
+                                            className={cx(
+                                              styles.smallPadding,
+                                              styles.hoverable
+                                            )}
+                                          >
+                                            {this.formatNum(
+                                              entry.shortHillsStudents
+                                            )}
+                                          </td>
+                                          <td
+                                            className={cx(
+                                              styles.smallPadding,
+                                              styles.hoverable
+                                            )}
+                                          >
+                                            {this.formatNum(
+                                              entry.shortHillsFacultyStaff
+                                            )}
+                                          </td>
+                                        </tr>
+                                      );
+                                    }
+                                  )}
+                                </tbody>
+                              </table>
+                            </div>
+                          </h3>
+                        </div>
+                      </div>
+                      {showMoreRowsButton}
+                      {showFewerRowsButton}
+                      <br />
+                      {showArchiveData}
+
+                      <div>
+                        Updated upon receipt of results from Mirimus Labs
+                      </div>
+                      <div>
+                        <a
+                          href="https://pingryanywhere.org/health-safety/covid-19-testing/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Detailed information regarding Pingry's COVID-19
+                          Testing
+                        </a>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                </Col>
+
+                <Col md={9} lg={7} className={styles.center}>
+                  <Card
+                    style={{
+                      display: this.state.priorTestingShown ? "flex" : "none",
+                    }}
+                  >
+                    <Card.Body>
+                      <h6 className={cx("mb-4", styles.smallmargin)}>
+                        COVID-19 Testing - Archived Data
                       </h6>
                       <div className="row d-flex align-items-center">
                         <div className="col-12">
@@ -203,7 +519,7 @@ class App extends Component {
                                         <tr
                                           style={{
                                             display:
-                                              i < 3 || this.state.showMore
+                                              i < this.state.archiveEntriesShown
                                                 ? "table-row"
                                                 : "none",
                                           }}
@@ -239,7 +555,8 @@ class App extends Component {
                         <br />
                         <br />
                       </div>
-                      {icon}
+                      {showMoreRowsArchiveButton}
+                      {showFewerRowsArchiveButton}
                       <div>
                         Updated upon receipt of results from Mirimus Labs
                       </div>
@@ -256,6 +573,8 @@ class App extends Component {
                     </Card.Body>
                   </Card>
                 </Col>
+                {/* <TestingChart testingData={this.state.fetchedTestingData} /> */}
+
                 <CampusCards
                   internal={this.state.internal}
                   detailedStats={this.state.detailedStats}
